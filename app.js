f6af40720767151f493cb68ea15a99b328a8ea5c
vlev322 <vlev322@gmail.com>
2019-02-21 17:44:07 +0200

    GraphQl API

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql')
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
 const {
 	buildSchema
-} = require('graphql')
-const app = express();
+} = require('graphql');
 
-const events = [];
+const Event = require('./models/event');
+const User = require('./models/user');
+
+const app = express();
 
 app.use(bodyParser.json())
 
@@ -18,11 +22,15 @@ app.use('/graphql', graphqlHttp({
 			description: String!
 			price: Float!
 			date: String!
+			creator: User!
 		}
 
-		type RootQuery {
-			events: [Event!]! 
-		}	
+		type User {
+			_id: ID!
+			email: String!
+			password: String
+			createdEvents: [Event!]
+		}
 		
 		input EventInput {
 			title: String!
@@ -31,8 +39,18 @@ app.use('/graphql', graphqlHttp({
 			date: String
 		}
 
+		input UserInput {
+			email: String!
+			password: String!
+		}
+		
+		type RootQuery {
+			events: [Event!]! 
+		}	
+
 		type RootMutation {
-			createEvent(eventInput: EventInput ): Event
+			createEvent(eventInput: EventInput): Event
+			createUser(userInput: UserInput): User
 		}
 
 		schema {
@@ -40,25 +58,95 @@ app.use('/graphql', graphqlHttp({
 			mutation: RootMutation
 		}
 	`),
+
 	rootValue: {
 		events: () => {
-			return events;
+			return Event.find()
+			.populate('creator')
+				.then(events => {
+					return events.map(event => {
+						return {
+							...event._doc,
+							_id: event.id
+						};
+					});
+				})
+				.catch(err => {
+					throw err;
+				});
 		},
-		createEvent: (args) => {
-			const event = {
-				_id: Math.random().toString(),
+		createEvent: args => {
+			const event = new Event({
 				title: args.eventInput.title,
 				description: args.eventInput.description,
 				price: +args.eventInput.price,
-				date: new Date().toISOString()
-			}
-			console.log(args);
-			
-			events.push(event);
-			return event;
+				date: new Date().toISOString(),
+				creator: '5c6ea577495cbd30a059e1a7'
+			});
+			let createdEvent;
+			return event
+				.save()
+				.then(result => {
+					createdEvent = {
+						...result._doc,
+						_id: result._doc._id.toString()
+					};
+					return User.findById('5c6ea577495cbd30a059e1a7');
+				})
+				.then(user => {
+					if (!user) {
+						throw new Error('User not found')
+					}
+					user.createdEvents.push(event);
+					return user.save()
+				})
+				.then(result => {
+					return createdEvent;
+				})
+				.catch(err => {
+					console.log(err);
+					throw err;
+				});
+
+		},
+		createUser: args => {
+			return User.findOne({
+					email: args.userInput.email
+				}).then(user => {
+					if (user) {
+						throw new Error('User exists already..!')
+					}
+					return bcrypt
+						.hash(args.userInput.password, 8)
+				}).then(hashedPassword => {
+					const user = new User({
+						email: args.userInput.email,
+						password: hashedPassword
+					});
+					return user.save();
+				})
+				.then(result => {
+					return {
+						...result._doc,
+						password: null,
+						_id: result.id
+					};
+				})
+				.catch(err => {
+					throw err
+				})
 		}
-	}, 
+	},
 	graphiql: true
 }));
 
-app.listen(3000);
+mongoose
+	.connect(
+		`mongodb+srv://${process.env.MONGO_USER}:${
+		process.env.MONGO_PASSWORD
+	}@mynewclaster-wg2ao.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
+	).then(() => {
+		app.listen(3000);
+	}).catch(err => {
+		console.log(err);
+	});
